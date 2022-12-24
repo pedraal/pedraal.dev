8d1b49ba543e10ff549b319dddcb6504e2bfda0c
pierre golfier <pgolfier.pro@gmail.com>
2022-12-24 16:56:21 +0100

    feat: implement dropdown and navbar burger menu

--- components/app/Navbar.vue
+++ components/app/Navbar.vue
@@ -8,7 +8,7 @@ const { navigation } = useContent()
       <NuxtLink to="/" class="font-handwritten hover:text-primary -mt-4 text-3xl transition-colors">
         <span class="font-handwritten inline-block -rotate-12 text-6xl">pg</span>
       </NuxtLink>
-      <div class="flex gap-2">
+      <div class="hidden gap-2 md:flex">
         <AppNavlink
           v-for="link of navigation"
           :key="link._path"
@@ -30,6 +30,25 @@ const { navigation } = useContent()
       <PrimerExternalLink href="mailto:pgolfier.pro@gmail.com" target="_blank">
         <Icon name="carbon:at" class="h-5 w-5" />
       </PrimerExternalLink>
+      <span class="md:hidden">|</span>
+      <PrimerDropdown class="md:hidden">
+        <template #button>
+          <span class="sr-only">Menu</span>
+          <svg aria-hidden="true" class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+          </svg>
+        </template>
+        <template #menu>
+          <NuxtLink
+            v-for="link of navigation"
+            :key="link._path"
+            :to="link._path"
+            class="font-handwritten"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </template>
+      </PrimerDropdown>
     </div>
   </nav>
 </template>
---
+++ components/primer/Dropdown.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const open = ref(false)
+
+const eventListener = () => open.value = false
+
+onMounted(() => {
+  document.addEventListener('click', eventListener)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', eventListener)
+})
+</script>
+
+<template>
+  <div class="relative">
+    <button class=" rounded-lg border border-neutral-500 p-2" @click.stop="open = !open">
+      <slot name="button" />
+    </button>
+
+    <Transition
+      enter-from-class="opacity-0 -translate-y-4"
+      enter-active-class="transition-all ease-out"
+      enter-to-class="opacity-1 -translate-y-0"
+      leave-from-class="opacity-1 -translate-y-0"
+      leave-active-class="transition-all ease-out"
+      leave-to-class="opacity-0 -translate-y-4"
+    >
+      <div v-if="open" class="absolute top-[calc(100%+0.5rem)] right-0 flex flex-col gap-3 rounded-lg bg-neutral-800 p-4 text-white shadow dark:bg-white dark:text-black">
+        <slot name="menu" />
+      </div>
+    </Transition>
+  </div>
+</template>
