3114df27ce7aa275af978c1a600f93a7ef11a71c
pierre golfier <pgolfier.pro@gmail.com>
2023-10-31 16:43:50 +0100

    feat: update fonts cdn and add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,14 +35,10 @@ export default defineNuxtConfig({
     head: {
       title: 'Pierre Golfier',
       link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
         {
-          rel: 'preconnect', href: 'https://fonts.googleapis.com',
-        },
-        {
-          rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous',
-        },
-        {
-          href: 'https://fonts.googleapis.com/css2?family=Indie+Flower&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', rel: 'stylesheet',
+          href: 'https://fonts.bunny.net/css?family=indie-flower:400|poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap', rel: 'stylesheet',
         },
       ],
       bodyAttrs: {
