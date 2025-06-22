85e272c3278d2115905f590e99031ae055c6d3cf
pedraal <pro@pedraal.fr>
2025-06-22 23:52:02 +0200

    fix: REVERT AND FIXME

--- server/routes/resume/cv-pierre-golfier.pdf.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
-  setResponseHeader(event, 'Content-Type', 'application/pdf')
-  setResponseHeader(event, 'Content-Disposition', 'inline; filename="cv-pierre-golfier.pdf"')
-
-  const pdf = await printPdf(query.theme === 'dark')
-
-  return Buffer.from(pdf)
-})
